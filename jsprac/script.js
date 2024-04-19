ebf29797f15268889bcc972c8782e259370b9055
Nithin Borusu <nithinborusu23@gmail.com>
2024-04-19 21:34:06 +0530

    intial commit

---
+++ jsprac/script.js
@@ -0,0 +1,19 @@
+//user registration
+let userName = "nithin";
+let full_Name = "nithin borusu";
+const eMail = "najdb@gmail.com";
+let ph = 8864356;
+console.log(typeof full_Name);
+console.log("my username is "+ userName+"phone number: "+ph);
+//interploation method
+console.log(`
+  My user name: ${userName}
+  phone number: ${ph}
+  `
+
+)
+let num1=9;
+let num2 =10;
+console.log(num1+num2);
+let a = null;
+console.log(typeof a);
